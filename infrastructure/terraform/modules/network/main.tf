b5af2bee6e9dbf3d5ea576934a4aeac24b710857
Zach-Maestas <zachary.maestas03@gmail.com>
2026-03-06 16:14:10 -0700

    feat(network): added VPC flow logs

--- infrastructure/terraform/modules/network/main.tf
+++ infrastructure/terraform/modules/network/main.tf
@@ -149,3 +149,63 @@ resource "aws_route" "private_internet_access" {
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.this[count.index].id
 }
+
+# --- VPC FLOW LOGS ---
+
+# CloudWatch Log Group for VPC Flow Logs
+resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
+  name              = "/aws/vpc/flow-logs/${var.project}"
+  retention_in_days = 7
+  tags              = { Name = "${var.project}-vpc-flow-logs" }
+}
+
+resource "aws_iam_role" "vpc_flow_logs" {
+  name = "${var.project}-vpc-flow-logs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "vpc-flow-logs.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = { Name = "${var.project}-vpc-flow-logs-role" }
+}
+
+resource "aws_iam_role_policy" "vpc_flow_logs" {
+  name = "${var.project}-vpc-flow-logs-policy"
+  role = aws_iam_role.vpc_flow_logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "WriteVPCFlowLogsToCloudWatch"
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams"
+        ]
+        Resource = "${aws_cloudwatch_log_group.vpc_flow_logs.arn}:*"
+      }
+    ]
+  })
+}
+
+# VPC Flow Log
+resource "aws_flow_log" "this" {
+  vpc_id               = aws_vpc.main.id
+  traffic_type         = "ALL"
+  log_destination_type = "cloud-watch-logs"
+  log_destination      = aws_cloudwatch_log_group.vpc_flow_logs.arn
+  iam_role_arn         = aws_iam_role.vpc_flow_logs.arn
+
+  tags = { Name = "${var.project}-vpc-flow-log" }
+}
